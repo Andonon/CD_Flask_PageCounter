6bcb187d55ba137fb57fe0730afb29cfeb72c784
Troy Center <troycenter1@gmail.com>
2017-06-10 10:28:50 -0700

    finsihed the page counter and tested. so easy!

--- counter.py
+++ counter.py
@@ -1,15 +1,23 @@
 '''Counter App will count how many times the root page has been displayed
 by Troy Center, Coding Dojo Python Flask fundamentals, using sessions'''
 
-#pylint: disable=c0106,c0111
+#pylint: disable=c0111,c0103
+from flask import Flask, render_template, url_for, request, redirect, session
 
-from flask import Flask, render_template, request, session
+app = Flask(__name__)
 
-app=Flask(__name__)
+app.secret_key = 'lkjas-0823lkasdasdasdas-478'
+
+# increment the variable for the session counter.
+# if it doesn't exit, create it. (first pass)
+def sessionCounter():
+    try:
+        session['counter'] += 1
+    except KeyError:
+        session['counter'] = 1
 
 @app.route('/')
-def count_page_hits(): 
-    print "homepage"
-    pass
-
+def homepage():
+    sessionCounter()
+    return render_template('counter.html')
 app.run(debug=True)
